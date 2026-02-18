bf568539f7c970ce5fc78284f016e7a120e4e23e
Soms <mesieromanders@gmail.com>
2026-02-18 18:42:59 +0200

    UpdateDatabase and CreateAllCRUD

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TabletosModule } from './tabletos/tabletos.module'; // Імпортуємо ваші таблетки
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-      TabletosModule, // <--- Підключаємо вашу функціональність сюди
+      TabletosModule,
+      UsersModule,
+      AuthModule, // <--- Підключаємо вашу функціональність сюди
       // Коли з'являться юзери, додасте: UsersModule
   ],
   controllers: [], // <--- Тут пусто, бо головний контролер нам не треба
--- apps/api/src/tabletos/tabletos.controller.ts
+++ apps/api/src/tabletos/tabletos.controller.ts
@@ -2,11 +2,14 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { TabletosService } from './tabletos.service';
 import { CreateTabletoDto } from './dto/create-tableto.dto';
 import { UpdateTabletoDto } from './dto/update-tableto.dto';
+import { UseGuards } from '@nestjs/common';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('tabletos')
 export class TabletosController {
   constructor(private readonly tabletosService: TabletosService) {}
 
+  @UseGuards(AuthGuard)
   @Post()
   create(@Body() createTabletoDto: CreateTabletoDto) {
     return this.tabletosService.create(createTabletoDto);
@@ -21,12 +24,12 @@ export class TabletosController {
   findOne(@Param('id') id: string) {
     return this.tabletosService.findOne(+id);
   }
-
+  @UseGuards(AuthGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateTabletoDto: UpdateTabletoDto) {
     return this.tabletosService.update(+id, updateTabletoDto);
   }
-
+  @UseGuards(AuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.tabletosService.remove(+id);
